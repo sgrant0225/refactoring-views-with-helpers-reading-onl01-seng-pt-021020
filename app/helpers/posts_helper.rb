0a890859251e5907e2e9ef96e19be985b05d2945
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-11 22:27:03 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   
  def last_updated(post)
-   updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+   post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
  end
 end
